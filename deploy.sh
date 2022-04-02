68de0048d42a44c091f86d4b39950cbf53c4770c
Gökhan Şengün <gokhansengun@gmail.com>
2022-04-02 03:32:40 +0300

    Generalize config

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,31 @@
 # on minikube
 # sudo ip link set docker0 promisc on
 
-for ((i=0;i<5;i++)); do
-  helm upgrade  --install redis-node-${i} \
+announceIP=192.168.59.121
+dataNodePortStart=30002
+redisNodeStr=""
+
+for ((i=0;i<=5;++i)); do
+  redisNodeStr="${redisNodeStr}${announceIP}:$((${dataNodePortStart} + ${i})) "
+done
+
+for ((i=0;i<=5;i++)); do
+  helm upgrade --install redis-node-${i} \
     --namespace=default \
     --create-namespace \
-    --values helm/values/redis-node-${i}.yaml \
+    --set=dataNodePort=$((${dataNodePortStart} + ${i})) \
+    --set=announceIP=${announceIP} \
+    --set=redisNodes="${redisNodeStr}" \
     helm/charts/redis-cluster-node
 done
 
-helm upgrade  --install redis-node-5 \
-  --namespace=default \
-  --create-namespace \
-  --values helm/values/redis-node-5.yaml \
-  helm/charts/redis-cluster-node
+for ((i=0;i<=5;i++)); do
+  helm upgrade --install redis-node-${i} \
+    --namespace=default \
+    --create-namespace \
+    --set=dataNodePort=$((${dataNodePortStart} + ${i})) \
+    --set=announceIP=${announceIP} \
+    --set=redisNodes="${redisNodeStr}" \
+    --set=reservePortsOnly=false \
+    helm/charts/redis-cluster-node
+done
